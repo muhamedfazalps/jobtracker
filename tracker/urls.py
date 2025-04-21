a4118a6d6df358d8bcb5e13e7b5196d53fb5f338
MUHAMED FAZAL PS <107547381+muhamedfazalps@users.noreply.github.com>
2025-04-21 18:42:18 +0530

    Update urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),  # Home route
     path('signup/recruiter/', views.recruiter_signup, name='recruiter_signup'),
     path('signup/jobseeker/', views.jobseeker_signup, name='jobseeker_signup'),
     path('login/', views.login_view, name='login'),
