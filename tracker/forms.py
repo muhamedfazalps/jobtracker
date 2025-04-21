63293ffd95152f598b68f6318e31b601f315c2ae
MUHAMED FAZAL PS <107547381+muhamedfazalps@users.noreply.github.com>
2025-04-21 18:26:52 +0530

    Update forms.py

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,10 +1,27 @@
 from django import forms
-from .models import JobApplication
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
-class JobForm(forms.ModelForm):
-    class Meta:
-        model = JobApplication
-        fields = '__all__'
-        widgets = {
-            'applied_on': forms.DateInput(attrs={'type': 'date'}),
-        }
+class RecruiterSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_recruiter = True
+        if commit:
+            user.save()
+        return user
+
+class JobSeekerSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_jobseeker = True
+        if commit:
+            user.save()
+        return user
